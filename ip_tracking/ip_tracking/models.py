2e51953d578ecc5e930bed009b9e23a1aaed6f98
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2026-01-15 17:24:28 +0200

    Implement IP logging middleware and model
    Added IPLoggingMiddleware to log client IP addresses and request paths.

--- ip_tracking/ip_tracking/models.py
+++ ip_tracking/ip_tracking/models.py
@@ -1,1 +1,61 @@
+# ip_tracking/models.py
+from django.db import models
 
+
+class RequestLog(models.Model):
+    ip_address = models.GenericIPAddressField()
+    path = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.path} - {self.timestamp}"
+
+# ip_tracking/middleware.py
+from .models import RequestLog
+
+
+class IPLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Get client IP address
+        ip_address = self.get_client_ip(request)
+
+        # Log request data
+        RequestLog.objects.create(
+            ip_address=ip_address,
+            path=request.path
+        )
+
+        response = self.get_response(request)
+        return response
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            return x_forwarded_for.split(',')[0]
+        return request.META.get('REMOTE_ADDR')
+
+# settings.py
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
+    'ip_tracking.middleware.IPLoggingMiddleware',
+
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INSTALLED_APPS = [
+    ...
+    'ip_tracking',
+]
+
+
+python manage.py makemigrations ip_tracking
+python manage.py migrate
